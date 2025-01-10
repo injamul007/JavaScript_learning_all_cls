6addb880c2eae54bfd1dc9c577db2bd5482f6b4f
injamul007 <injamul007hoque@gmail.com>
2025-01-10 08:58:32 +0600

    first commit

---
+++ learning-JS-Class-1/js-Math-Object/index.js
@@ -0,0 +1,24 @@
+//! Math Object---
+// console.log(Math.PI);
+// console.log(Math.max(60,30,24,600,712,89));
+// console.log(Math.min(60,30,24,600,712,89));
+// console.log(Math.round(1.4));
+// console.log(Math.ceil(1.3));
+// console.log(Math.floor(1.7));
+// console.log(Math.random(2));
+// console.log(Math.abs(-5));
+// console.log(Math.sqrt(5));
+// console.log(Math.pow(2,10));
+
+//! Date Object----
+
+let currDate = new Date('26 september 1993');
+console.log(currDate);
+
+let newDate = new Date(1993,8,26);
+// console.log(newDate);
+
+// console.log(newDate.getFullYear());
+console.log(newDate.setFullYear(2022));
+
+console.log(newDate);
