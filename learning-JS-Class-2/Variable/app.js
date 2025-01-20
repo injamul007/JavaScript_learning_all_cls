40668c75d4cfdedab9ed1fb8b65bbb75408576c1
injamul007 <injamul007hoque@gmail.com>
2025-01-20 17:57:21 +0600

    first commit

---
+++ learning-JS-Class-2/Variable/app.js
@@ -0,0 +1,46 @@
+//? Javascript Variables
+/*
+* JavaScript variable names are case-sensitive
+! Variable names cannot contain spaces
+* Variable cannot be the same as reserved keywords such as 'if' or 'const'.
+* By convention, JavaScript variable names are written in camelCase.
+* Variable should be given descriptive names that indicate their content and usage (e.g.`sellingPrice` and `costPrice` rather than `x` and `y`).
+* You cannot add number first
+* _, $ is Acceptable but - not Acceptable
+*/
+
+
+/*
+* let Variable
+! You cannot re declare
+* You can re assign
+? Block Scooped
+! Let is not hosted
+*/
+
+// let b = 10;
+// b = 11;
+// {
+//   console.log(b);
+// }
+// console.log(people);
+// let people = 'someone';
+
+// let fruits = 'Jelly';
+// console.log(`${fruits} is my fav, ${fruits} i like to eat`);
+
+
+
+/*
+* const Variable
+! You cannot re declare
+! You can not re assign
+? Block Scooped
+! const is not hosted
+*/
+
+// const c = 10;
+// c = 12;
+
+
+
