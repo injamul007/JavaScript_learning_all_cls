eea89e440949b321b7abc22c99544616b8d6f1bd
injamul007 <injamul007hoque@gmail.com>
2025-01-24 18:38:35 +0600

    first commit

---
+++ learning-JS-Class-2/JavaScriptMath/app.js
@@ -0,0 +1,25 @@
+// Javascript Math
+
+let num = 30.9999990001;
+let num2 = 2;
+//if needed round number
+
+// console.log(Math.round(num));
+// console.log(Math.ceil(num));
+// console.log(Math.floor(num));
+
+
+//Square Value
+// console.log(Math.pow(3,2));
+
+//Root Value
+// console.log(Math.sqrt(64));
+// console.log(Math.abs(-5));
+// console.log(Math.min(1,3,4,5,6,7,0));
+// console.log(Math.max(1,3,4,5,6,7,0));
+console.log(Math.PI);
+
+//Random Number
+// console.log(Math.random().toFixed(2));
+// console.log(Math.random().toFixed(2) * 10 + 1);
+console.log(Math.floor(Math.random().toFixed(2) * 10 + 1));
