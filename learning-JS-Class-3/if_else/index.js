335983ccb1daa59206b77f11d25f56ef3b32cff4
injamul007 <injamul007hoque@gmail.com>
2025-01-25 10:07:34 +0600

    first commit

---
+++ learning-JS-Class-3/if_else/index.js
@@ -0,0 +1,151 @@
+
+// var num =  6;
+
+// if (num % 2 == 0) {
+//   console.log("even");
+// } 
+// if (num % 2 != 0) {
+//   console.log("odd");
+// }
+
+
+
+// var num = prompt("Enter a number : ");
+// if (num > 0) 
+//     console.log("Positive");
+
+// else if (num < 0)
+//     console.log("Negative");
+
+
+// else
+//     console.log("Zero");
+
+
+
+// var marks = prompt("Enter your marks : ")
+
+// if (marks > 100 || marks < 0)
+//   console.log("Invalid marks");
+
+// else if (marks >= 80 && marks <= 100)
+//   console.log("You got A+");
+
+// else if ( marks >= 70 && marks <=79)
+//   console.log("You got A");
+
+// else if (marks >= 60 && marks <= 69)
+//   console.log("You got A-");
+
+// else if (marks >= 50 && marks <= 59)
+//   console.log("You got B");
+
+// else if (marks >= 40 && marks <= 49)
+//   console.log("You got C");
+
+// else if (marks >= 33 && marks <= 39)
+//   console.log("You just Pass");
+
+// else
+//   console.log("You Failed");
+
+
+// var num1 = prompt("Enter a num1 : ");
+// var num2 = prompt("Enter a num2 : ");
+// var num3 = prompt("Enter a num3 : ");
+
+
+// if (num1>num2 && num1>num3)
+//   console.log("Large number = " + num1);
+
+// else if (num2>num1 && num2>num3)
+//   console.log("Large number = " + num2);
+
+// else
+//   console.log("Large number = " + num3);
+
+
+//? Vowel / consonant
+
+// var letter = prompt("Enter a letter : ");
+
+// letter = letter.toLowerCase();
+
+// if (letter=="a" || letter=="e" || letter=="i" || letter=="o" || letter=="u")
+//   console.log("Vowel");
+
+// else
+//   console.log("Consonant");
+
+
+
+//Digit spelling
+//0-zero, 1 - one........9-nine, 10 - not a valid digit
+// switch, case, break, default
+
+// var digit = prompt("Enter any digit : ");
+
+// switch (digit) {
+//   case "0":
+//     console.log("zero");
+//     break;
+//     case "1":
+//     console.log("one");
+//     break;
+//     case "2":
+//     console.log("two");
+//     break;
+//     case "3":
+//     console.log("three");
+//     break;
+//     case "4":
+//     console.log("four");
+//     break;
+//     case "5":
+//     console.log("five");
+//     break;
+//     case "6":
+//     console.log("six");
+//     break;
+//     case "7":
+//     console.log("seven");
+//     break;
+//     case "8":
+//     console.log("eight");
+//     break;
+//     case "9":
+//     console.log("nine");
+//     break;
+
+//     default: 
+//     console.log("Not a digit");
+// }
+
+
+
+// var letter = prompt("Enter a letter : ");
+
+// letter = letter.toLowerCase();
+
+// switch (letter) {
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//       console.log("Vowel");
+//       break;
+
+//     default: 
+//     console.log("consonant");
+// }
+
+
+  //->Starting; Condition; Updating----------
+for (let x = 10; x >= 1; x = x - 1) {
+  // document.write("<h1> Bangladesh </h1>");
+  // document.write("" + x);
+}
+
+
+
