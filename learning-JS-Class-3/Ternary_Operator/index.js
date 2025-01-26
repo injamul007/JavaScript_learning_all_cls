144f18fc083f28aa5b4e46201b6227a48a8f4c16
injamul007 <injamul007hoque@gmail.com>
2025-01-26 16:12:44 +0600

    first commit

---
+++ learning-JS-Class-3/Ternary_Operator/index.js
@@ -0,0 +1,20 @@
+//? Ternary Operator
+var number = Number(prompt("Enter a number "));
+// if (number > 0) {
+//   console.log("Positive");
+// }
+// else if (number < 0){
+//   console.log("Negative");
+// }
+// else{
+//   console.log("Zero");
+// }
+
+
+var result = number > 0 ? "Positive" : number < 0 ? "Negative" : "Zero";
+console.log(result);
+
+
+
+// var result = number > 0 ? "Positive" : "Negative"
+// console.log(result);
