fdd2aa8cba03a1623075851129fec68e17deb29b
injamul007 <injamul007hoque@gmail.com>
2025-03-06 16:14:15 +0600

    first commit

---
+++ learning-JS-Class-5/Arrays/app.js
@@ -0,0 +1,48 @@
+// Arrays
+// let fruits = ["mango", "apple", "litchi"];
+// console.log(fruits);
+// fruits[0] = "banana";
+// console.log(fruits);
+
+//Array Method
+//! Push : add to end 
+//! Pop : delete from end & returns it
+//! Unshift : add to start
+//! Shift : delete from start & returns it
+//! Slice : to slice the arrays(copies a portion of an array)
+//! Splice : removes / replace / add elements in place(multiple arguments)(make changes on original portion of an array)(similar to slice but has a little bit different)(it has 3 parameters - start,delete,item in and out)
+//! Sort Method : it follows ascending/descending order(alphabetically shorted)
+
+
+
+// let cars = ["audi", "bmw", "mercedes", "xuv"];
+// cars.push("toyota");
+// console.log(cars);
+// cars.pop();
+// console.log(cars);
+// cars.unshift("sonata");
+// console.log(cars);
+// cars.shift();
+// console.log(cars);
+
+// let followers = ["a","b","c","d"];
+// let blocked = followers.shift();
+// console.log(followers);
+// console.log(blocked);
+
+
+let week = ["wednesday", "friday", "monday", "sunday", "tuesday", "saturday", "thursday"];
+console.log(week);
+
+// Array References
+// Address in memory
+
+
+//constant variable
+
+
+//Nested array
+
+
+
+
