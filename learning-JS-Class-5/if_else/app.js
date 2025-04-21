f8693be215e2bb709a2dbb3dc1536b9da55c719f
injamul007 <injamul007hoque@gmail.com>
2025-04-21 21:52:22 +0600

    first commit

---
+++ learning-JS-Class-5/if_else/app.js
@@ -0,0 +1,74 @@
+// let pencilPrice = 10;
+// let eraserPrice = 5;
+// let output = `The total price is : ${pencilPrice + eraserPrice} Taka`;
+// console.log(output);
+
+
+//Conditional Statement
+// console.log("before my if statement");
+// let age = 16;
+// if (age >= 18) {
+//   console.log("You can vote");
+//   console.log("You can drive");
+// }
+// if (age <= 18) {
+//   console.log("You cannot vote or drive");
+// }
+
+// let firstName = "tonmoy";
+// if (firstName == "tonmoy") {
+//   console.log(`Welcome ${firstName}`);
+// }
+
+//! PQ?
+// let color = "green";
+
+//Traffic Light System
+// if (color === "red") {
+//   console.log("Please stop");
+// }
+// else if (color === "green") {
+//   console.log("Please going");
+// }
+// else if (color === "yellow") {
+//   console.log("Please slowdown");
+// }else {
+//   console.log("Traffic light is broken");
+// }
+
+// let month = "january";
+
+// if (month === "january") {
+//   console.log("winter is here");
+// } else if (month === "april") {
+//   console.log("summer is here");
+// }
+
+// !PQ2----
+
+// let size = "s";
+
+// if (size === "xl") {
+//   console.log("Popcorn price is 250 taka");
+// } else if (size === "l") {
+//   console.log("Popcorn price is 200 taka");
+// } else if (size === "m") {
+//   console.log("Popcorn price is 100 taka");
+// } else if (size === "s") {
+//   console.log("Popcorn price is 50 taka");
+// } else {
+//   console.log("Popcorn is no longer to sell");
+// }
+
+let marks = 45;
+
+if(marks >= 33) {
+  console.log("Pass");
+  if (marks >= 80) {
+    console.log("outstanding");
+  } else {
+    console.log("A")
+  }
+} else {
+  console.log ("Better luck next time");
+}
