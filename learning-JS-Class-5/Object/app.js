9ded78d56a130a85324a668106a2282106a710e2
injamul007 <injamul007hoque@gmail.com>
2025-05-07 15:35:15 +0600

    first commit

---
+++ learning-JS-Class-5/Object/app.js
@@ -0,0 +1,80 @@
+//!  Object
+// used to store collections & complex entities.
+
+// const student = {
+//   name: "tonmoy",
+//   age: 30,
+//   marks: 70,
+//   city: "dhaka"
+// };
+
+// let delhi ={
+//   latitude: "28.7041 N",
+//   longitude: "77.1025 E"
+// };
+
+// const item = {
+//   price: 100.99,
+//   discount: 50,
+//   colors: ["red", "blue"]
+// };
+
+// thread / twitter post
+
+// const post = {
+//     username: "@tonmoy",
+//     content: "This is my #firstPost",
+//     likes: 150,
+//     reposts: 5,
+//     tags: ["@apnacollege", "@delta"]
+// };
+
+//! nested object
+
+// const classInfo = {
+//   aman: {
+//     grade: "A+",
+//     city: "Delhi"
+//   },
+//   shradha: {
+//     grade: "A",
+//     city: "Pune"
+//   },
+//   karan: {
+//     grade: "O",
+//     city: "Mumbai"
+//   }
+// };
+
+//! Array of Objects
+
+// const classInfo = [
+//   {
+//     name: "aman",
+//     grade: "A+",
+//     city: "Delhi"
+//   },
+//   {
+//     name: "shradha",
+//     grade: "A",
+//     city: "Pune"
+//   },
+//   {
+//     name: "karan",
+//     grade: "O",
+//     city: "Mumbai"
+//   }
+// ];
+
+//! Math Object
+//! Properties
+// Math.PI
+// Math.E
+//! Method
+// Math.abs(n)
+// Math.pow(a,b)
+// Math.floor(n) -- Nearest smallest int value;
+// Math.ceil(n) -- Nearest largest int value;
+// Math.round(n) --
+// Math.random(n) -- Between 0 and 1 it gives random value;
+
