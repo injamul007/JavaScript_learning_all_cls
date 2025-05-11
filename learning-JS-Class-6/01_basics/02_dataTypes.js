06dccd52912dbfcaab6d22f212d8b1de405075fc
injamul007 <injamul007hoque@gmail.com>
2025-05-11 13:30:09 +0600

    first commit

---
+++ learning-JS-Class-6/01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict" //treat all js code as newer version.
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+  + 
+    3) // code readability should be high
+
+console.log("MyName") // this is a readable code.
+
+let name = "Boss"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//! Data Types in JS
+//! These are primitive data type.
+// number //? 12345
+// bigint //? bigNum and at the last you have to add "n"
+// string //? " hello" 
+// boolean //? True / False
+//undefined //? that anything is/has not defined/assigned yet
+//null //? StandAlone Value  // empty
+// symbol  //? characters of symbols//for making things unique
+
+//! These are NON-Primitive Data type also call Object.
+//Object //? Stored data with key pairs value
+//Array //? Collection of store data
+
+
+console.log(typeof undefined); //? undefined
+console.log(typeof null); //? object
+
