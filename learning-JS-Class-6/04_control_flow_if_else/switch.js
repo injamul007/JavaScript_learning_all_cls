17a00ba3655858dc935dda59196e4579d96a1a55
injamul007 <injamul007hoque@gmail.com>
2025-06-08 18:36:17 +0600

    first commit

---
+++ learning-JS-Class-6/04_control_flow_if_else/switch.js
@@ -0,0 +1,43 @@
+//***************************** Switch *******************************************
+
+// const month = 3;
+
+// switch (month) {
+//   case 1:
+//     console.log("january");
+//     break;
+//   case 2:
+//     console.log("february");
+//     break; //? This is most important thing in switch, because if you do not use break keyword here then everything will be executed from here except default option.
+//   case 3:
+//     console.log("March");
+//     break;
+//   case 4:
+//     console.log("april");
+//     break;
+
+
+//   default:
+//     break;
+
+
+const month = "february";
+
+switch (month) {
+  case "jan":
+    console.log("january");
+    break;
+  case "february":
+    console.log("february");
+    break; //? This is most important thing in switch, because if you do not use break keyword here then everything will be executed from here except default option.
+  case "march":
+    console.log("March");
+    break;
+  case "april":
+    console.log("april");
+    break;
+
+
+  default:
+    break;
+}
