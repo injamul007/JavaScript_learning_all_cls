dad87e52952d6e88c2020edee82f0c011097b5bb
injamul007 <injamul007hoque@gmail.com>
2025-06-16 18:38:17 +0600

    first commit

---
+++ learning-JS-Class-6/05_iterations/two.js
@@ -0,0 +1,23 @@
+//************************ While Loop ********************************
+
+let index = 0
+while (index <= 10) {
+  // console.log(`Value of index is ${index}`);
+    index = index + 2;
+  }
+  
+let myArray = ["flash", "thor", "hulk", "superman"]
+  let arr = 0
+
+while (arr < myArray.length) {
+  // console.log(`Value of index is ${arr} and ${myArray[arr]}`)
+  arr++
+}
+
+let score = 11
+
+do { //? first one execution is must happen here.
+  console.log (`Score is ${score}`);
+  score++
+} while (score <= 10)
+
