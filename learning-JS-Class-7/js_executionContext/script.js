564e4ca3d1582c90817b952dd518015f4fd07ae5
injamul007 <injamul007hoque@gmail.com>
2025-07-13 00:23:33 +0600

    first commit

---
+++ learning-JS-Class-7/js_executionContext/script.js
@@ -0,0 +1,22 @@
+//? Execution Context of JS***********
+
+// function sayName() {
+//   var name = "someName";
+//   console.log("the name is, ", name);
+// };
+
+//? Breaking the functions into pieces is called tokenization.
+//? AST(Abstract Syntax Tree).
+//? Machine Code.
+
+
+//? Global Executional context(GEC);
+//?1. Creating Phase. -> 1. window: global object. 2.this:window
+//?2. Execution Phase.
+
+
+//? Stack and Heap Memory system******
+
+//? Stack -> Stack is a Data-Structure (LIFO- Last in First Out)
+//? Heap -> Heap is a Discrete Memory Structure
+
